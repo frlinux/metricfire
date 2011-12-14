ce597eda9db1c27d35213428a3fc21c8771625ef
FRLinux <frlinux@frlinux.eu>
2011-12-14 17:02:20 +0100

    added more metrics

--- sysmetricfire.py
+++ sysmetricfire.py
@@ -4,10 +4,12 @@
 # license. 
 #
 # FRLinux - frlinux@frlinux.net
+# Thanks to John Hamill for the help on Python :)
 #
 # you need psutil v0.4 or above - http://code.google.com/p/psutil/
 #
 # v0.1 - 13/12/11 - basic metrics feeding to metricfire
+# v0.2 - 14/12/11 - more metrics gathering based on psutil
 
 import time
 import psutil
@@ -37,3 +39,20 @@ for mem in mems:
 	metric = getattr(virtmemusage,mem)
    	metricfire.send("virtmem." + mem, str(metric))
 
+disks = psutil.disk_usage('/')
+partitions = ['total', 'used', 'free', 'percent' ]
+
+for partition in partitions:
+        metric = getattr(disks,partition)
+        metricfire.send("disk." + partition, str(metric))
+
+interface = psutil.network_io_counters('eth0')
+#print type(interface),dir(interface)
+nics = ['bytes_sent', 'bytes_recv']
+
+for eth,iostats in interface.iteritems():
+        if "eth0" in eth:
+                for nic in nics:
+                        metric = getattr(iostats,nic)
+                        metricfire.send("eth0." + nic, metric)
+                                                                                                              
